5e0deb3601f66c13d8a54cf3167f0fc04a997450
Hiroshi Furuya <furuyahir@gmail.com>
2021-03-04 23:37:08 -0500

    Add in interfaces for procedures/timeline classes

---
+++ EYESOFTHEPEGASUS/Assets/Scripts/Procedures/Procedure.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+// TODO: Fill out this class
+public class Procedure : ITimeline
+{
+    public ITimelineItem CurrentItem()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public LinkedList<ITimelineItem> Timeline()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public ITimelineItem RemoveTimelineItem(ITimelineItem timelineItem)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public ITimelineItem AddTimelineItem(ITimelineItem timelineItemToAdd, ITimelineItem previousTimelineItem)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public int CurrentTimelineCost(CostType costType)
+    {
+        throw new System.NotImplementedException();
+    }
+}
