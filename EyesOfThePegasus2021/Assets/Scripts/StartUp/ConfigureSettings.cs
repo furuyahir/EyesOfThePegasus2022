489fd166f3f00cd02057c11b2d1e52fc276724a9
Raiffa Syamil <ra596961@ucf.edu>
2021-03-17 10:43:52 -0400

    setting up startup scene and scripts

---
+++ EyesOfThePegasus2021/Assets/Scripts/StartUp/ConfigureSettings.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigureSettins : MonoBehaviour
+{
+    UnityEngine.TouchScreenKeyboard keyboard;
+    public static string keyboardText = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, false, false);
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TouchScreenKeyboard.visible == false && keyboard != null)
+        {
+            if (keyboard.status == TouchScreenKeyboard.Status.Done)
+            {
+                keyboardText = keyboard.text;
+                keyboard = null;
+            }
+            if(!PlayerPrefs.HasKey("TelemetryURL"))
+            {
+                PlayerPrefs.SetString("TelemtryURL", keyboardText);
+            }
+            
+        }
+    }
+}
